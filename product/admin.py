a3b7d1613fc18fd1fd9d4648cf355c7f4500d516
Bskasan <b.kasan@yandex.com>
2023-07-08 22:50:10 +0300

    changed title, header and index title

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.site_title = 'RoninDev'
+admin.site.site_header = 'Admin Panel - RoninDev'
+admin.site.index_title = 'RoninDev - Index Page'
+
+
 # Register your models here.
 admin.site.register(Product)
